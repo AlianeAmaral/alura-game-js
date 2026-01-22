bca8f5b6b76749e1df84ad11d62f96cad0721ec7
Aliane <alianeamaral.tec@gmail.com>
2026-01-22 09:10:18 -0400

    refactor: adiciona centralização de valores, prevenção de magic numbers

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 alert("Bem-vindo(a) ao Game do Número Secreto.");
 
-let numeroSecreto = parseInt(Math.random() * 20 + 1); //20 porque são duas casas decimais para esquerda e + 1 pois considera 0, para ter o 20 precisa dele.
+let numeroMaximo = 20;
+
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); //20 porque são duas casas decimais para esquerda e + 1 pois considera 0, para ter o 20 precisa dele.
 console.log(`O número secreto é ${numeroSecreto}.`); //apenas para testar qual é o número secreto via inspecionar.
 
 let palpite;
 let tentativas = 1; //a primeira tentativa já está sendo feita quando o usuário insere o número.
 
 while (palpite != numeroSecreto) {
-    let palpite = prompt("Escolha um número entre 1 e 20:");
+    palpite = prompt(`Escolha um número entre 1 e ${numeroMaximo}:`);
 
     if (numeroSecreto == palpite) {
         break; //o break fará com que se o usuário acertar na primeira tentativa, a lógica vá para fora do while.
     }
     else { //aqui é certeza que o usuário errou, então deve contar a tentativa.
         if (palpite < numeroSecreto) {
-            alert(`O número secreto é maior do que ${palpite}.`)
+            alert(`O número secreto é maior do que ${palpite}.`);
         }
         else {
-            alert(`O número secreto é menor do que ${palpite}.`)
+            alert(`O número secreto é menor do que ${palpite}.`);
         }
         tentativas++ //ou tentativas = tentativas + 1; é ele + ele mesmo.
     }
