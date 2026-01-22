10da7c74a762ea9abeafaad05e998402eb4924f4
Aliane <alianeamaral.tec@gmail.com>
2026-01-21 22:00:53 -0400

    improvement: adiciona estrutura de repetição para o processo de adivinhação

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 alert("Bem-vindo(a) ao Game do Número Secreto.");
+let numeroSecreto = 7;
+let palpite;
 
-let numeroSecreto = 1;
-let palpite = prompt("Escolha um número entre 1 e 20:");
+while (palpite != numeroSecreto) {
+    let palpite = prompt("Escolha um número entre 1 e 20:");
 
-if (numeroSecreto == palpite) {
-    alert(`Parabéns! Você acertou o número secreto, que era: ${numeroSecreto}.`);
-}
-
-else {
-    if (palpite < numeroSecreto) {
-        alert(`O número secreto é maior do que ${palpite}.`)
+    if (numeroSecreto == palpite) {
+        alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto}.`);
     }
     else {
-        alert(`O número secreto é menor do que ${palpite}.`)
+        if (palpite < numeroSecreto) {
+            alert(`O número secreto é maior do que ${palpite}.`)
+        }
+        else {
+            alert(`O número secreto é menor do que ${palpite}.`)
+        }
     }
-
-    
 }
