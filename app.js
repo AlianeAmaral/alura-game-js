ccde8223d254d6bbfa75a19385156b05f3fa1c3f
Aliane <alianeamaral.tec@gmail.com>
2026-01-22 07:50:40 -0400

    refactor: ajusta posição e descrição dos comentários

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 alert("Bem-vindo(a) ao Game do Número Secreto.");
-let numeroSecreto = parseInt(Math.random() * 20) + 1; //número aleatório entre 1 e 20.
+
+let numeroSecreto = parseInt(Math.random() * 20 + 1); //20 porque são duas casas decimais para esquerda e + 1 pois considera 0, para ter o 20 precisa dele.
+console.log(`O número secreto é ${numeroSecreto}.`); //apenas para testar qual é o número secreto via inspecionar.
+
 let palpite;
 let tentativas = 1; //a primeira tentativa já está sendo feita quando o usuário insere o número.
 
@@ -7,20 +10,16 @@ while (palpite != numeroSecreto) {
     let palpite = prompt("Escolha um número entre 1 e 20:");
 
     if (numeroSecreto == palpite) {
-        //o break fará com que se o usuário acertar na primeira tentativa, ele vai para fora do while.
-        break;
+        break; //o break fará com que se o usuário acertar na primeira tentativa, a lógica vá para fora do while.
     }
-    //aqui é certeza que o usuário errou.
-    else {
+    else { //aqui é certeza que o usuário errou, então deve contar a tentativa.
         if (palpite < numeroSecreto) {
             alert(`O número secreto é maior do que ${palpite}.`)
         }
         else {
             alert(`O número secreto é menor do que ${palpite}.`)
         }
-
-        //tentativas = tentativas + 1; é o mesmo que está abaixo.
-        tentativas++
+        tentativas++ //ou tentativas = tentativas + 1; é ele + ele mesmo.
     }
 }
 
@@ -28,6 +27,7 @@ let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
 
 alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto}, com ${tentativas} ${palavraTentativa}.`);
 
+//tudo que foi substituído pelo uso do operador ternário:
 //if (tentativas > 1) {
 //    alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto}, com ${tentativas} tentativas.`);
 //}
